7d724f21595314aa5f85f7e23c64cc972d675f09
Tconan99 <627594232@qq.com>
2020-05-27 01:35:44 +0800

    ...

--- Codeforces 1349B - Orac and Medians.py
+++ Codeforces 1349B - Orac and Medians.py
@@ -19,12 +19,7 @@ def printd(value):
     # print(value)
     pass
 
-for _ in range(t):
-    arr = list(map(int, input().split(" ")))
-    (n, k) = arr[0], arr[1]
-    s = list(map(int, input().split(" ")))
-
-
+def run(n, k, s):
     i = 0
     isSuccess = True if len(s) == 1 and s[0] == k else False
     while i < n:
@@ -43,8 +38,7 @@ for _ in range(t):
                 blow += 1
 
             if biger - blow == 0 or biger - blow == 1:
-                isSuccess = True
-                break
+                return True
 
             if s[pos] == k:
                 break
@@ -52,40 +46,17 @@ for _ in range(t):
             pos += 1
 
         i = pos
-        
-        if isSuccess:
-            break
+    return isSuccess
 
-    if not isSuccess:
-        s = s[::-1]
-        i = 0
-        while i < n:
-            if s[i] != k:
-                i += 1
-                continue
-            
-            blow = 0
-            biger = 0
-            pos = i + 1
-            while pos < n:
+def case():
+    arr = list(map(int, input().split(" ")))
+    (n, k) = arr[0], arr[1]
+    s = list(map(int, input().split(" ")))
 
-                if s[pos] >= k:
-                    biger += 1
-                else:
-                    blow += 1
-
-                if biger - blow == 0 or biger - blow == 1:
-                    isSuccess = True
-                    break
-
-                if s[pos] == k:
-                    break
-
-                pos += 1
-            
-            i = pos
-            
-            if isSuccess:
-                break
+    isSuccess = run(n, k, s)
+    isSuccess = isSuccess or run(n, k, s[::-1])
     
     print("yes" if isSuccess else "no")
+
+for _ in range(t):
+    case()
