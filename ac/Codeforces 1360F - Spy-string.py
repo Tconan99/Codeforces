03bd0b59102cc50a79552ece24841934d5419a22
xueep <xueep@jcinfo.online>
2020-05-26 13:37:14 +0800

    1360F

---
+++ ac/Codeforces 1360F - Spy-string.py
@@ -0,0 +1,58 @@
+# https://codeforces.com/contest/1360/problem/F
+
+import sys
+import os
+import heapq
+import math
+
+try:
+    path = "./file/input.txt"
+    if os.path.exists(path):
+        sys.stdin = open(path, 'r')
+    # sys.stdout = open(r"./file/output.txt", 'w')
+except:
+    pass
+
+t = int(input())
+
+def printd(value):
+    # print(value)
+    pass
+
+for _ in range(t):
+    arr = list(map(int, input().split(" ")))
+    n, m = arr[0], arr[1]
+    a = []
+    for _ in range(n):
+        a.append(input())
+
+    result = -1
+
+    for i in range(m):
+
+        success = True
+        chars = []
+        for row in range(1, n):
+            count = 0
+            for pos in range(m):
+                if pos != i:
+                    if a[0][pos] != a[row][pos]:
+                        count += 1
+            if count > 1:
+                success = False
+                break
+            elif count == 1:
+                if a[row][i] not in chars:
+                    chars.append(a[row][i])
+        if not success:
+            continue
+
+        if len(chars) == 1:
+            result = a[0]
+            result = result[0:i] + chars[0] + result[i + 1:]
+            break
+        elif len(chars) == 0:
+            result = a[0]
+            break
+    
+    print(result)
